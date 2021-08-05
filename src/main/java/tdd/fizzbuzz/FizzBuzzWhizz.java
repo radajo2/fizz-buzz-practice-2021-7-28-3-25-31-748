9d02d23181bcb17b192d5b6e3e4cf8e455eaa915
RADAJO2 <radajo2@oocl.com>
2021-08-05 13:49:43 +0800

    [FizzBuzzWhizz][Joanna] feat: should_return_fizz_when_count_off_given_order_number_multiple_of_3 - pass

--- src/main/java/tdd/fizzbuzz/FizzBuzzWhizz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzzWhizz.java
@@ -3,7 +3,9 @@ package tdd.fizzbuzz;
 public class FizzBuzzWhizz {
 
     public String countoff(int orderNumber) {
-
+        if (orderNumber%3 == 0){
+            return "Fizz";
+        }
         return String.valueOf(orderNumber);
 
     }
